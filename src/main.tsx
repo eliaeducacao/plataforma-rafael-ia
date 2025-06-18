51962d72387c0fe51a49416d7abf637c1e4d1162
Henrique Costa <henrqiuecostapsilva@gmail.com>
2025-06-18 02:38:35 -0300

    feat: integra o Toaster e organiza rotas com AuthProvider e ProtectedRoute
    - Adiciona o componente Toaster para notificações.
    - Envolve as rotas em AuthProvider para gerenciar o estado de autenticação.
    - Implementa ProtectedRoute para rotas privadas de chat e agentes.

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,36 @@ import { createRoot } from 'react-dom/client';
 
 import { Route, Switch } from 'wouter';
 
-import './index.css';
+import { Toaster } from 'sonner';
+
+import '@/index.css';
 
 import HomePage from '@/modules/home/pages/home';
-import { LoginPage } from '@/modules/auth/pages/login';
+import LoginPage from '@/modules/auth/pages/login';
+import ChatPage from '@/modules/chat/pages';
+import AgentsPage from '@/modules/agents/pages';
+
+import { ProtectedRoute } from '@/modules/auth/components/protected-route';
+import { AuthProvider } from '@/modules/auth/contexts/auth-context';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Switch>
-      <Route path="/" component={HomePage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/auth/callback">
-          <div>Verificando autenticação...</div>
-      </Route>,
-    </Switch>
+    <AuthProvider>
+      <Toaster position='top-center' duration={3000} richColors />
+      <Switch>
+        {/* Rotas Públicas */}
+        <Route path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/auth/callback">
+            <div>Verificando autenticação...</div>
+        </Route>
+
+        {/* Rotas Privadas */}
+        <ProtectedRoute>
+          <Route path="/chat" component={ChatPage}/>
+          <Route path="/agents" component={AgentsPage} />
+        </ProtectedRoute>
+      </Switch>
+    </AuthProvider>
   </StrictMode>
 );
