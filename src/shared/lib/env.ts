88f61d8da4eec0532b67cd2dfaad61841f372c12
Henrique Costa <henriquecostapsilva@gmail.com>
2025-08-08 18:09:52 -0300

    feat: atualiza esquema de variáveis de ambiente
    - Remove variáveis de ambiente relacionadas ao Supabase e OpenAI do arquivo env.ts.
    - Adiciona a variável VITE_API_BASE_URL com mensagem de validação.
    - Ajusta a formatação das importações para consistência.

--- src/shared/lib/env.ts
+++ src/shared/lib/env.ts
@@ -1,33 +1,8 @@
-import { z } from "zod/v4-mini";
+import { z } from 'zod/v4-mini';
 
 export const envSchema = z.object({
-  VITE_SUPABASE_URL: z.url({
-    message: "URL do Supabase inválida"
-  }),
-  VITE_SUPABASE_KEY: z.string({
-    message: "Chave anônima do Supabase é obrigatória"
-  }),
-  VITE_SMTP_HOST: z.string({
-    message: "Host do SMTP é obrigatório"
-  }),
-  VITE_SMTP_PORT: z.coerce.number({
-    message: "Porta do SMTP deve ser um número"
-  }),
-  VITE_SMTP_USER: z.string({
-    message: "Usuário do SMTP é obrigatório"
-  }),
-  VITE_SMTP_PASSWORD: z.string({
-    message: "Senha do SMTP é obrigatória"
-  }),
-  VITE_SMTP_NAME: z.string({
-    message: "Nome do remetente é obrigatório"
-  }),
-  VITE_SMTP_EMAIL: z.email({
-    message: "Email do remetente inválido"
-  }),
-
-  VITE_OPENAI_KEY: z.string({
-    message: "Chave da API OpenAI é obrigatória"
+  VITE_API_BASE_URL: z.url({
+    message: 'URL da API é obrigatória',
   }),
 });
 
